5edf83caf9a10ecfca29b593f9e3ff1b4ba4f23d
James Wright <james@jamesswright.co.uk>
2017-10-10 18:03:41 +0100

    Fix benchmarking logging issue

--- test/benchmarking/captureBenchmarks.js
+++ test/benchmarking/captureBenchmarks.js
@@ -9,11 +9,11 @@ const BENCHMARK_COUNT = MAX_WORKER_COUNT - (MIN_WORKER_COUNT - 1);
 const SORTER_PATH = path.join(__dirname, '..', 'sorter', 'index.js');
 const NUMBERS_PATH = path.join(__dirname, '..', 'sorter', 'numbers-large.json');
 
-const getWorkerText = workerCount => `worker${workerCount > 1 ? 's' : ''}`;
+const getWorkerText = workerCount => `${workerCount} worker${workerCount > 1 ? 's' : ''}`;
 
 const runBenchmark = async workerCount => { // TODO: rerun a few times and take average
-    console.log(`Testing with ${workerCount + 1} ${getWorkerText(workerCount + 1)}...`);
-    const queue = createQueue(SORTER_PATH, workerCount + 1);
+    console.log(`Testing with ${getWorkerText(workerCount)}...`);
+    const queue = createQueue(SORTER_PATH, workerCount);
     const time = process.hrtime();
     await queue.schedule({ filename: NUMBERS_PATH });
     const [seconds, nanoseconds] = process.hrtime(time);
@@ -27,7 +27,7 @@ const sequentialRangeAsync = async (length, predicate) => {
     const results = Array(length);
 
     for (let i = 0; i < length; i++) {
-        results[i] = await predicate(i);
+        results[i] = await predicate(i + 1); // one-based due to its consumption
     }
 
     return results;
@@ -37,7 +37,7 @@ const printResults = results => (
     console.log(
         '\n\nResults\n',
         results.map(
-            res => `${getWorkerText(res.workerCount + 1)}: ${res.seconds}.${res.nanoseconds} secs`
+            res => `${getWorkerText(res.workerCount)}: ${res.seconds}.${res.nanoseconds} secs`
         ).join('\n')
     )
 );
