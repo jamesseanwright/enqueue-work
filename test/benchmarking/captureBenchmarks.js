3493c1e9f7f17c946dac0efed6561a090d4f9455
James Wright <james@jamesswright.co.uk>
2017-10-11 21:08:54 +0100

    WIP send payloads in parallel

--- test/benchmarking/captureBenchmarks.js
+++ test/benchmarking/captureBenchmarks.js
@@ -5,17 +5,19 @@ const { createQueue } = require('../../lib');
 
 const MIN_WORKER_COUNT = 1;
 const MAX_WORKER_COUNT = 8;
+
 const BENCHMARK_COUNT = MAX_WORKER_COUNT - (MIN_WORKER_COUNT - 1);
+const BENCHMARK_ITERATION_COUNT = 10;
 const SORTER_PATH = path.join(__dirname, '..', 'sorter', 'index.js');
 const NUMBERS_PATH = path.join(__dirname, '..', 'sorter', 'numbers-large.json');
 
 const getWorkerText = workerCount => `${workerCount} worker${workerCount > 1 ? 's' : ''}`;
 
-const runBenchmark = async workerCount => { // TODO: rerun a few times and take average
+const runBenchmark = async workerCount => { // TODO: run with constant payloads
     console.log(`Testing with ${getWorkerText(workerCount)}...`);
     const queue = createQueue(SORTER_PATH, workerCount);
     const time = process.hrtime();
-    await queue.schedule({ filename: NUMBERS_PATH });
+    await runParallel(BENCHMARK_ITERATION_COUNT, async() => await queue.schedule({ filename: NUMBERS_PATH }));
     const [seconds, nanoseconds] = process.hrtime(time);
 
     queue.kill();
@@ -23,7 +25,11 @@ const runBenchmark = async workerCount => { // TODO: rerun a few times and take 
     return { workerCount, seconds, nanoseconds };
 };
 
-const sequentialRangeAsync = async (length, predicate) => {
+const runParallel = (length, predicate) => (
+    Promise.all(Array(length).fill(null).map(async () => await predicate()))
+);
+
+const runSequentially = async (length, predicate) => {
     const results = Array(length);
 
     for (let i = 0; i < length; i++) {
@@ -45,6 +51,6 @@ const printResults = results => (
 
 (async () => {
     console.log('Capturing benchmarks...');
-    const results = await sequentialRangeAsync(BENCHMARK_COUNT, runBenchmark);
+    const results = await runSequentially(BENCHMARK_COUNT, runBenchmark);
     printResults(results);
 })();
